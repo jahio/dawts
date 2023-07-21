8313b790bec67749df1b945d2b04bba59098489a
J. Austin Hughey <github@jah.io>
2023-07-20 22:15:14 -0500

    More elegant OMP prompt initiation, use non-spotify addled prompt (+ misc config tweaks)

--- powershell/hax/prompt/oh-my-posh.ps1
+++ powershell/hax/prompt/oh-my-posh.ps1
@@ -1,10 +1,29 @@
 # Use oh-my-posh (https://ohmyposh.dev/) for shell prompt
-$desired_posh_theme = "plague"
 
-# TODO: Make sure desired theme file exists
-$themePath = "$(brew --prefix oh-my-posh)/themes/$desired_posh_theme.omp.json"
+function Get-PoshTheme($themeName) {
+  # TODO: Make this look for the .toml, .yml and .yaml equivalents too
+  $themePath = "$(brew --prefix oh-my-posh)/themes/$themeName.omp.json"
+  if (Test-Path $themePath) {
+      return $themePath
+  }
+  else {
+      Write-Host "Theme $themeName not found"
+      return $null
+  }
+}
 
-oh-my-posh --init --shell pwsh --config $themePath | Invoke-Expression
-
+function Set-PoshTheme($desired_posh_theme) {
+  oh-my-posh --init --shell pwsh --config "$(Get-PoshTheme($desired_posh_theme))" | Invoke-Expression
+}
 # Set PSReadline options to show a menu on tab complete
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+
+# Trigger the theme to be set
+Set-PoshTheme("tonybaloney")
+# Set-PoshTheme("bubbles")
+# Set-PoshTheme("fish")
+# Set-PoshTheme("craver")
+# Set-PoshTheme("jblab_2021")
+# Set-PoshTheme("multiverse-neon")
+# Set-PoshTheme("sonicboom_dark")
+# Set-PoshTheme("iterm2")
