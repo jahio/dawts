727ec1b12428a2aaf04970e9be9aca7fb96be5d6
J. Austin Hughey <git@jah.io>
2026-05-12 17:10:44 -0400

    Spoof curl user agent for ipinfo.io since they intentionally broke usage for powershell

--- powershell/hax/util/get-myip.ps1
+++ powershell/hax/util/get-myip.ps1
@@ -9,7 +9,9 @@
 #
 
 function Get-MyIP {
-  $rawIP = Invoke-WebRequest 'http://ipinfo.io' -ContentType 'application/json' -Method Get -DisableKeepAlive -Headers @{'Accept' = 'application/json'}
+  # 2025-05-12: Found that ipinfo.io is throwing http 503's if it detects pwsh user agent from scripting.
+  # Changing user agent to curl evades that problem. They broke it for powershell intentionally but not for curl. Provable fact.
+  $rawIP = Invoke-WebRequest 'http://ipinfo.io' -ContentType 'application/json' -Method Get -DisableKeepAlive -Headers @{'Accept' = 'application/json'; 'User-Agent' = 'curl/8.7.1'}
   $MyIP = $rawIP.Content | ConvertFrom-Json | Select-Object -ExcludeProperty readme
   $MyIP
 }
